5c6d5a65eaae9f9745a76cb2cc0b71e3cb085e61
david <d.mai@studenti.unibg.it>
2023-03-16 14:04:36 +0100

    Added Giocatori principal method

--- Group_14_My_Shelfie/src/main/Giocatori.java
+++ Group_14_My_Shelfie/src/main/Giocatori.java
@@ -1,5 +1,27 @@
 package main;
 
 public class Giocatori {
-
+	private String nome;
+	private String cognome;
+	private int punteggio=0;
+	private int numero_giocatore;
+	private int contatore_giocatori;
+	public void SetNomeGiocatore(String nome) {
+		this.nome=nome;
+	}
+	
+	public void SetCognomeGiocatore(String cognome) {
+		this.cognome=cognome;
+	}
+	
+	public void AumentaPunteggioGiocatore(int aumento) {
+		this.punteggio+=aumento;
+	}
+	
+	public void Giocatore() {
+		if(contatore_giocatori<=4) {
+			this.numero_giocatore=contatore_giocatori;
+			contatore_giocatori++;
+		}
+	}
 }
