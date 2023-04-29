047ae191e9251e513abb31795bf21d67385f4e86
Riccardo Maffeis <r.maffeis4@studenti.unibg.it>
2023-04-29 11:24:03 +0200

    add getCella in libreria and some changes

--- Group_14_My_Shelfie/src/main/Libreria.java
+++ Group_14_My_Shelfie/src/main/Libreria.java
@@ -1,34 +1,58 @@
 package main;
 
+import java.util.Scanner;
+
 public class Libreria {
-	public int riga=6,colonna=5;
+	public int riga,colonna;
 	
-	CellaLibreria [][] arraylibreria = new CellaLibreria [6][5];
+	CellaLibreria [][] libreria = new CellaLibreria [6][5];
 	TesseraOggetto[][] tesseraoggetto = new TesseraOggetto[6][5];
-	public String Selezione (int riga ,int colonna) {
-		this.riga = riga;
-		this.colonna = colonna;
-		String contenuto = new String();
-		contenuto = arraylibreria[riga][colonna].contenuto;
-		return contenuto;
+		
+	//funzione che restituisce il tipo di tessera presente nella cella	
+	public TesseraOggetto getTessera(int riga, int colonna) {
+		TesseraOggetto tesseracontrollo = TesseraOggetto.VUOTA;
+			
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.CORNICE) {
+			tesseracontrollo = TesseraOggetto.CORNICE;
+		}
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.GATTO) {
+			tesseracontrollo = TesseraOggetto.GATTO;
+		}
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.TROFEO) {
+			tesseracontrollo = TesseraOggetto.TROFEO;
+		}
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.GIOCO) {
+			tesseracontrollo = TesseraOggetto.GIOCO;
+		}
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.LIBRO) {
+			tesseracontrollo = TesseraOggetto.LIBRO;
+		}
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.PIANTA) {
+			tesseracontrollo = TesseraOggetto.PIANTA;
+		}
+		if(tesseraoggetto[riga][colonna] == TesseraOggetto.VUOTA) {
+			tesseracontrollo = TesseraOggetto.VUOTA;
+		}
+		return tesseracontrollo;
 	}
+	
 	//serie di funzioni per rendere la cella della libreria una determinata tessera oggetto
-		public void SetCellaTrofeo(int riga, int colonna) {
-			tesseraoggetto[riga][colonna] = TesseraOggetto.TROFEO;
-		}
-		public void SetCellaPianta(int riga, int colonna) {
-			tesseraoggetto[riga][colonna] = TesseraOggetto.PIANTA;
-		}
-		public void SetCellaGatto(int riga, int colonna) {
-			tesseraoggetto[riga][colonna] = TesseraOggetto.GATTO;
-		}
-		public void SetCellaGioco(int riga, int colonna) {
-			tesseraoggetto[riga][colonna] = TesseraOggetto.GIOCO;
-		}
-		public void SetCellaCornice(int riga, int colonna) {
-			tesseraoggetto[riga][colonna] = TesseraOggetto.CORNICE;
-		}
-		public void SetCellaLibro(int riga, int colonna) {
-			tesseraoggetto[riga][colonna] = TesseraOggetto.LIBRO;
-		}
+	public void SetCellaTrofeo(int riga, int colonna) {
+		tesseraoggetto[riga][colonna] = TesseraOggetto.TROFEO;
+	}
+	public void SetCellaPianta(int riga, int colonna) {
+		tesseraoggetto[riga][colonna] = TesseraOggetto.PIANTA;
+	}
+	public void SetCellaGatto(int riga, int colonna) {
+		tesseraoggetto[riga][colonna] = TesseraOggetto.GATTO;
+	}
+	public void SetCellaGioco(int riga, int colonna) {
+		tesseraoggetto[riga][colonna] = TesseraOggetto.GIOCO;
+	}
+	public void SetCellaCornice(int riga, int colonna) {
+		tesseraoggetto[riga][colonna] = TesseraOggetto.CORNICE;
+	}
+	public void SetCellaLibro(int riga, int colonna) {
+		tesseraoggetto[riga][colonna] = TesseraOggetto.LIBRO;
+	}
 }
