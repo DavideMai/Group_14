db04ffccd868bdfddb5271e10a52cbc80283a850
MicheleCaprinali <m.caprinali@studenti.unibg.it>
2023-04-30 10:42:13 +0200

    Class ObiettivoComune updating

---
+++ Group_14_My_Shelfie/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ObiettivoComune ob= new ObiettivoComune();
+		
+		System.out.println(ob.PrimaCarta());
+		System.out.println(ob.SecondaCarta());
+		
+		
+	}
+
+}
