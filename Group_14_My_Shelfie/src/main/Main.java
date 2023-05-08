c9a78ff197ecbfcaa774f92bf9d63d5ea00a681e
MicheleCaprinali <m.caprinali@studenti.unibg.it>
2023-05-08 18:08:40 +0200

    test main

--- Group_14_My_Shelfie/src/main/Main.java
+++ Group_14_My_Shelfie/src/main/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		//prova carte comune
+		//test
 		/*ObiettivoComune ob= new ObiettivoComune();
 		
 		System.out.println(ob.PrimaCarta());
