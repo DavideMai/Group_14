a5f0e5e673fcab8df36f60835f56e3bfc86d8ac7
DavideMai <d.mai@studenti.unibg.it>
2023-05-11 13:07:26 +0200

    Updates to imports

--- Group_14_My_Shelfie/src/partita/Giocatori.java
+++ Group_14_My_Shelfie/src/partita/Giocatori.java
@@ -6,19 +6,7 @@ import java.util.Random;
 import java.util.Set;
 
 import obiettivi.*;
-import obiettivi_comuni.DecimoObiettivoComune;
-import obiettivi_comuni.DodicesimoObiettivoComune;
-import obiettivi_comuni.NonoObiettivoComune;
-import obiettivi_comuni.ObiettivoComune;
-import obiettivi_comuni.OttavoObiettivoComune;
-import obiettivi_comuni.PrimoObiettivoComune;
-import obiettivi_comuni.QuartoObiettivoComune;
-import obiettivi_comuni.QuintoObiettivoComune;
-import obiettivi_comuni.SecondoObiettivoComune;
-import obiettivi_comuni.SestoObiettivoComune;
-import obiettivi_comuni.SettimoObiettivoComune;
-import obiettivi_comuni.TerzoObiettivoComune;
-import obiettivi_comuni.UndicesimoObiettivoComune;
+import obiettivi_comuni.*;
 import utils.TesseraOggetto;
 
 public class Giocatori {
