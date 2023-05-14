90fab3aff4015d36258d718ff05598b799293485
MicheleCaprinali <m.caprinali@studenti.unibg.it>
2023-05-14 16:34:27 +0200

    added method for common goal number 11

--- Group_14_My_Shelfie/src/obiettivi_comuni/UndicesimoObiettivoComune.java
+++ Group_14_My_Shelfie/src/obiettivi_comuni/UndicesimoObiettivoComune.java
@@ -13,7 +13,22 @@ public class UndicesimoObiettivoComune extends ObiettivoComune {
 
 	@Override
 	public boolean ControlloObiettivoComune(Libreria libreria) {
-		// TODO Auto-generated method stub
+		
+		for(int i=0; i<5; i++) //riga
+		{
+			
+			for(int j=0; j<4; j++) //colonna
+			{
+				if(libreria.getTessera(i,j)!= null && libreria.getTessera(i+1, j+1)!=null && libreria.getTessera(i+2, j+2)!=null && libreria.getTessera(i,j+2)!=null && libreria.getTessera(i+2, j)!=null) //controllo che la diagonale non sia vuota
+				{
+					if(libreria.getTessera(i,j)==libreria.getTessera(i+1,j+1) && libreria.getTessera(i,j)==libreria.getTessera(i+2,j+2) && libreria.getTessera(i,j)== libreria.getTessera(i+2,j) && libreria.getTessera(i,j)==libreria.getTessera(i,j+2)) //controllo che le tessere della diagonale siano uguali
+					{
+						return true; //condizione verificata
+					}
+				}
+					
+			}
+		}
 		return false;
 	}
 
