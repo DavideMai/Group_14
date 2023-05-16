8832223929813f94a790b3f330769dd1ef3eb916
MicheleCaprinali <m.caprinali@studenti.unibg.it>
2023-05-16 17:42:33 +0200

    added function for 5 and 8 common goal

--- Group_14_My_Shelfie/src/obiettivi_comuni/OttavoObiettivoComune.java
+++ Group_14_My_Shelfie/src/obiettivi_comuni/OttavoObiettivoComune.java
@@ -26,7 +26,67 @@ public class OttavoObiettivoComune extends ObiettivoComune {
 	@Override
 	public boolean ControlloObiettivoComune(Libreria libreria)
 	{
+		int contatore=0;
+		for(int i=0; i<6; i++)
+		{
+			int[] ntessere= new int[6];
+			boolean controllo=false;
+			for(int j=0; j<5; j++)
+			{
+				if(libreria.getTessera(i, j)!=null)
+				{
+					if(libreria.getTessera(i, j)==TesseraOggetto.CORNICE)
+					{
+						ntessere[0]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.GATTO)
+					{
+						ntessere[1]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.GIOCO)
+					{
+						ntessere[2]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.LIBRO)
+					{
+						ntessere[3]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.PIANTA)
+					{
+						ntessere[4]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.TROFEO)
+					{
+						ntessere[5]++;
+					}
+				}else {
+					controllo=true;
+					break;
+				}
+				
+			}
+			if(controllo!=true)
+			{
+				int contatore2=0;
+				for(int k=0; k<6; k++)
+				{
+					if(ntessere[k]==0)
+					{
+						contatore2++;
+					}
+				}
+				if(contatore2>=3)
+				{
+					contatore++;
+				}
+				if(contatore>=4)
+				{
+					return true;
+				}
+			}
+		}
 		return false;
+	}
 	}
 		
 		
--- Group_14_My_Shelfie/src/obiettivi_comuni/QuintoObiettivoComune.java
+++ Group_14_My_Shelfie/src/obiettivi_comuni/QuintoObiettivoComune.java
@@ -1,6 +1,7 @@
 package obiettivi_comuni;
 
 import partita.Libreria;
+import utils.TesseraOggetto;
 
 public class QuintoObiettivoComune extends ObiettivoComune {
 
@@ -13,7 +14,65 @@ public class QuintoObiettivoComune extends ObiettivoComune {
 
 	@Override
 	public boolean ControlloObiettivoComune(Libreria libreria) {
-		// TODO Auto-generated method stub
+		int contatore=0;
+		for(int j=0; j<5; j++)
+		{
+			int[] ntessere= new int[6];
+			boolean controllo=false;
+			for(int i=0; i<6; i++)
+			{
+				if(libreria.getTessera(i, j)!=null)
+				{
+					if(libreria.getTessera(i, j)==TesseraOggetto.CORNICE)
+					{
+						ntessere[0]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.GATTO)
+					{
+						ntessere[1]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.GIOCO)
+					{
+						ntessere[2]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.LIBRO)
+					{
+						ntessere[3]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.PIANTA)
+					{
+						ntessere[4]++;
+					}
+					else if(libreria.getTessera(i, j)==TesseraOggetto.TROFEO)
+					{
+						ntessere[5]++;
+					}
+				}else {
+					controllo=true;
+					break;
+				}
+				
+			}
+			if(controllo!=true)
+			{
+				int contatore2=0;
+				for(int k=0; k<6; k++)
+				{
+					if(ntessere[k]==0)
+					{
+						contatore2++;
+					}
+				}
+				if(contatore2>=3)
+				{
+					contatore++;
+				}
+				if(contatore>=3)
+				{
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	
